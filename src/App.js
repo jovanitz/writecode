3949ef09970f17fd609af39bc1caa85b5798c591
joshua <jovanitz@gmail.com>
2019-01-22 12:59:43 -0600

    agrego el menu web y mobile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Root, Routes } from 'react-static'
-import { Link } from '@reach/router'
+import ScrollAnimation from 'react-animate-on-scroll';
+import Menu from '@material-ui/icons/Menu';
+import "animate.css/animate.min.css";
+import './main.scss'
 
-import './app.scss'
+class App extends Component {
 
-function App() {
-  return (
-    <Root>
-      <nav>
-        <ul>
-          <li><a href="#inicio">Inicio</a></li>
-          <li><a href="#inicio">¿Que hacemos?</a></li>
-          <li><a href="#inicio">¿Quienes somos?</a></li>
-          <li><a href="#inicio">Contacto</a></li>
-        </ul>
-      </nav>
-      <div className="content">
-        <Routes />
-      </div>
-    </Root>
-  )
+  constructor() {
+    super();
+    this.state = {
+      showMenu: false,
+    }
+  }
+  
+  onclick() {
+    
+  }
+
+  render() {
+    const { showMenu } = this.state;
+    const classNameNav = showMenu ? 'nav-bg' : 'nav-bg nav-bg--off';
+    const classNameContent = showMenu ? 'filter-blur' : '';
+    const classNameHamburger = !showMenu ? 'hamburger' : 'hamburger hamburger--off';
+
+    return (
+      <Root>
+        <div className={ classNameHamburger } onClick={ () => this.setState({ showMenu: true }) }>
+          <Menu fontSize="large" />
+        </div>
+        <div onClick={ () => this.setState({ showMenu: false }) }>
+          <ScrollAnimation className={ classNameNav } animateIn="fadeIn">
+            <nav>
+              <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#inicio">¿Que hacemos?</a></li>
+                <li><a href="#inicio">¿Quienes somos?</a></li>
+                <li><a href="#inicio">Contacto</a></li>
+              </ul>
+            </nav>
+          </ScrollAnimation>
+        </div>
+        <div className={ classNameContent }>
+          <Routes />
+        </div>
+      </Root>
+    )
+  }
 }
 
 export default App
